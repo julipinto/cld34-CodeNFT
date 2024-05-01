177fc17019d948d65c3eac70e70c248c5a78a3a5
Juliana <aragaopintojuli@gmail.com>
2024-05-01 01:24:48 -0300

    stats and top nfts

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -1,6 +1,26 @@
+import './styles.css'
+
+const stats = [
+  {num: '300', stat: 'coleções'},
+  {num: '+100k', stat: 'usuários'},
+  {num: '+30k', stat: 'negociações'},
+  {num: '+1M', stat: 'visualizações'},
+]
+
 function Stats() {
   return (
-    <section></section>
+    <section className='stats glassy_bg g1 gradient_border'>
+      <div>
+        {stats.map((s, i) => {
+          return (
+            <div key={`stats-${i}`} className={`stats-${i}`}>
+              <strong>{s.num}</strong>
+              <span>{s.stat}</span>
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
