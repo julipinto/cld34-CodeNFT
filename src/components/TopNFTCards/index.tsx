703b5bc04d56258c9dd22914f05af50cfbf74831
Juliana <aragaopintojuli@gmail.com>
2024-05-03 20:31:42 -0300

    assets

--- src/components/TopNFTCards/index.tsx
+++ src/components/TopNFTCards/index.tsx
@@ -7,9 +7,9 @@ import { useGSAP } from "@gsap/react";
 import { useRef } from 'react';
 
 const topNFTs = [
-  '/public/assets/image 1.png',
-  '/public/assets/image 5.png',
-  '/public/assets/image 6.png'
+  '/assets/image 1.png',
+  '/assets/image 5.png',
+  '/assets/image 6.png'
 ]
 
 const id = 'topNFTs'
